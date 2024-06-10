bdddc6ba0e7a5c72ea80466d298656d5f8d9bfe0
Deev Savla <savla.deev@gmail.com>
2024-06-10 14:38:58 +0530

    Created template for Api Error and Response

---
+++ config/database.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () =>{
+    try{
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log(`Connected with database ${mongoose.connection.host}.`)
+    } catch(error) {
+
+    }
+}
