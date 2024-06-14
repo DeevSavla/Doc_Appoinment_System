285e7a523b66b3722a81cf656bb7214ce859f2ff
Deev Savla <savla.deev@gmail.com>
2024-06-14 10:36:21 +0530

    added the main page

--- client/src/components/Changepassword.jsx
+++ client/src/components/Changepassword.jsx
@@ -29,7 +29,7 @@ function Changepassword() {
 
     return (
         <Form className="w-1/3 mx-auto bg-white shadow-md rounded-lg p-6 my-36" onFinish={handleSubmit}>
-      <h1 className="text-center text-2xl mb-5 text-teal-600">Change Password</h1>
+      <h1 className="text-center text-2xl mb-5 text-blue-600">Change Password</h1>
       <Form.Item label="Email" name="email">
         <Input type="email" required />
       </Form.Item>
@@ -40,7 +40,7 @@ function Changepassword() {
         <Input type="password" required />
       </Form.Item>
       <div className="flex justify-between">
-        <button className="bg-teal-600 text-white px-4 py-2 rounded-lg hover:bg-teal-500 flex-grow">Submit</button>
+        <button className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-500 flex-grow">Submit</button>
       </div>
     </Form>
     )
--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import hero1 from '../photos/hero-img01.png'
+import hero2 from '../photos/hero-img02.png'
+import hero3 from '../photos/hero-img03.png'
 
 function MainPage() {
   return (
-    <div>MainPage</div>
+    <div className='flex justify-center py-8 px-4 ml-14'>
+      <div className='flex flex-col lg:flex-row items-center gap-8 max-w-screen-xl w-full'>
+        <div className='flex-1 text-center lg:text-left'>
+          <h1 className='text-4xl font-bold text-gray-800 mb-4'>We help patients live a healthy, longer life.</h1>
+          <p className='text-gray-600 mb-8'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus quaerat cumque fugit, perspiciatis cum nemo aperiam, aut quia earum amet architecto, modi odio. Soluta unde ducimus perferendis?
+          </p>
+          <div className='mt-8 flex flex-col lg:flex-row justify-center lg:justify-start items-center gap-8'>
+            <div className='text-center'>
+              <h1 className='text-4xl font-bold text-blue-600'>30+</h1>
+              <p className='text-gray-600'>Years of Experience</p>
+            </div>
+            <div className='text-center'>
+              <h1 className='text-4xl font-bold text-purple-600'>15+</h1>
+              <p className='text-gray-600'>Clinic Location</p>
+            </div>
+            <div className='text-center'>
+              <h1 className='text-4xl font-bold text-green-600'>100%</h1>
+              <p className='text-gray-600'>Patient Satisfaction</p>
+            </div>
+          </div>
+        </div>
+
+        <div className='flex-1 flex flex-col lg:flex-row items-center gap-4'>
+          <img src={hero1} className='w-72 h-96 rounded-lg shadow-lg object-cover' alt='Doctor' />
+          <div className='flex flex-col gap-4'>
+            <img src={hero2} className='w-36 h-48 rounded-lg shadow-lg object-cover' alt='Doctor' />
+            <img src={hero3} className='w-36 h-48 rounded-lg shadow-lg object-cover' alt='Doctor' />
+          </div>
+        </div>
+      </div>
+    </div>
+
   )
 }
 
