8d2a541b6913b87ced66d1dc8eba664a65359d28
Deev Savla <savla.deev@gmail.com>
2024-06-15 16:25:58 +0530

    extracted user data using redux

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -2,17 +2,22 @@ import React,{useEffect} from 'react'
 import { sideBarMenu } from '../Data/data'
 import { Link, useLocation } from 'react-router-dom'
 import logo from '../photos/logo.png'
-import axios from 'axios'
+import { useSelector} from 'react-redux'
+import Spinner from './Spinner'
 
 function Layout({ children }) {
 
   const location = useLocation()
-
+  const { user } = useSelector(state => state.user)
+  if(!user) {
+    return (<Spinner/>)
+  }
+  else{
   return (
     <div className="flex min-h-screen bg-gray-100 p-4 gap-6">
       <aside className="w-1/5 flex flex-col bg-blue-600 text-white rounded-lg shadow-md">
         <header className="text-center text-2xl font-bold p-4 bg-blue-700 rounded-t-lg">
-        <img src={logo} alt="Logo" className='h-10' />
+          <img src={logo} alt="Logo" className='h-10' />
         </header>
         <nav className="flex-grow p-2 space-y-2">
           {sideBarMenu.map((menu, index) => {
@@ -22,8 +27,8 @@ function Layout({ children }) {
                 key={index}
                 to={menu.path}
                 className={`flex items-center p-3 rounded-md transition-colors duration-200 ${isActive
-                    ? 'bg-teal-200 text-blue-800 font-semibold shadow-sm'
-                    : 'hover:bg-blue-500'
+                  ? 'bg-teal-200 text-blue-800 font-semibold shadow-sm'
+                  : 'hover:bg-blue-500'
                   }`}
               >
                 <i className={`mr-3 ${menu.icons} ${isActive ? 'text-blue-800' : 'text-white'}`}></i>
@@ -38,18 +43,15 @@ function Layout({ children }) {
 
       <main className="w-4/5 flex flex-col gap-6">
         <header className="h-16 bg-white rounded-lg shadow-sm p-4 flex items-center border border-gray-300">
-          header
+          {user.username}
         </header>
         <section className="flex-grow bg-white rounded-lg shadow-sm p-6 border border-gray-300">
           {children}
         </section>
       </main>
     </div>
-
-
-
-
-  )
+  );
+}
 }
 
 export default Layout
--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,8 +1,43 @@
 import React, { useEffect } from 'react'
 import { Navigate } from 'react-router-dom'
-
+import { useSelector, useDispatch } from 'react-redux'
+import { showloading, hideloading } from '../store/features/alertSlice'
+import axios from 'axios'
+import { setUser} from '../store/features/userSlice'
 
 function ProtectedRoute({ children }) {
+
+    const dispatch = useDispatch()
+    const { user } = useSelector(state => state.user)
+  
+    const getData = async () => {
+      try {
+        dispatch(showloading())
+        const res = await axios.post('http://localhost:8080/api/v1/user/getUserData', {
+          token: localStorage.getItem('token')
+        }, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+          }
+        })
+        dispatch(hideloading())
+        if (res.data) {
+          dispatch(setUser(res.data.data))
+        } else {
+          <Navigate to='/login' />
+        }
+      } catch (error) {
+        dispatch(hideloading())
+        localStorage.clear()
+        console.log(error)
+      } 
+    }
+  
+    useEffect(() => {
+      if (!user) {
+        getData();
+      }
+    }, [user])
 
     if (localStorage.getItem('token')) {
         return children;
