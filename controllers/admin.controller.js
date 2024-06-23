f502a84d8eb881a2b9270c98705605cd2c7d8891
Deev Savla <savla.deev@gmail.com>
2024-06-23 15:06:25 +0530

    made controller for users and doctors

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -7,11 +7,26 @@ import { asyncHandler } from '../utility/AsyncHandler.js'
 const getAllDoctorsController = asyncHandler(async (req,res)=>{
     const doctors = await Doctor.find({})
     
+    if(doctors) {
+        res.status(201).send(
+            new ApiResponse(200,doctors,'All Doctors Fetched.')
+        )
+    } else {
+        throw new ApiError(400,'No Doctors Found.')
+    }
 
 })
 
 const getAllUsersController = asyncHandler(async (req,res)=>{
-
+    const users = await User.find({})
+    
+    if(users) {
+        res.status(201).send(
+            new ApiResponse(200,users,'All Users Fetched.')
+        )
+    } else {
+        throw new ApiError(400,'No Users Found.')
+    }
 })
 
 export {
