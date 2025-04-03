cfb141ac98b0e92d8f5661c2a9b5cac40a9df107
Deev Savla <143065962+DeevSavla@users.noreply.github.com>
2025-04-03 20:40:26 +0530

    change port and url

--- client/src/utilities/baseUrl.js
+++ client/src/utilities/baseUrl.js
@@ -1,2 +1,2 @@
-//export const baseUrl = 'https://doc-appoinment-system.onrender.com'
-export const baseUrl = 'http://localhost:3000'
+export const baseUrl = 'https://doc-appoinment-system.onrender.com'
+//export const baseUrl = 'http://localhost:8080'
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(morgan('dev'))
 
 app.use(cors({
-  origin: ['http://localhost:5173','http://localhost:3000','https://doc-appoinment-system.vercel.app'], 
+  origin: ['http://localhost:5173','http://localhost:8080','https://doc-appoinment-system.vercel.app'], 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
