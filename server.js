9b3b9fbf57732d41462a0eddb55c72939476bfc7
Deev Savla <143065962+DeevSavla@users.noreply.github.com>
2025-04-03 15:39:26 +0530

    handle cors error

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(morgan('dev'))
 
 // Configure CORS to allow requests from frontend
 app.use(cors({
-  origin: ['http://localhost:5173','http://localhost:8080'], // Your frontend URL
+  origin: ['http://localhost:5173','http://localhost:8080','https://doc-appoinment-system.onrender.com'], // Your frontend URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
